209fa20e9f46d0358eca0a0c1794f98a97ec96f9
MaRcOoS <leha.chalov@mail.ru>
2023-06-22 23:37:47 +0300

    Домашняя работа

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿// Console.WriteLine("Введите число ");
+
+// int N = Convert.ToInt32(Console.ReadLine());
+// int number = 0;
+// if (N < 0 )
+// {
+//     Console.Write("Введите положительное число ");
+// }
+// if (N % 2 == 1)
+// {
+//     N --;
+// }
+// while (number <= N)
+// {
+//     Console.WriteLine(number);
+//     number = number + 2;
+// }
+
+
+// Console.WriteLine("Введите число ");
+
+// int Num = Convert.ToInt32(Console.ReadLine());
+
+// if (Num % 2 == 0)
+// {
+//     Console.WriteLine("Ваше число " + Num + " является четным");
+// }
+// if (Num % 2 == 1) 
+// {
+//     Console.WriteLine("Ваше число " + Num + " НЕ является четным");
+// }
+// if (Num % 2 == -1)
+// {
+//     Console.WriteLine("Ваше число " + Num + " НЕ является четным");
+// }
+
+
+
+
+// Console.WriteLine("Введите три числа ");
+// int Num1 = Convert.ToInt32(Console.ReadLine());
+// int Num2 = Convert.ToInt32(Console.ReadLine());
+// int Num3 = Convert.ToInt32(Console.ReadLine());
+// int max = Num1;
+// if (Num1 < Num2)
+// {
+//     max = Num2;
+// }
+// if (Num2 < Num3)
+// {
+//     max = Num3;
+// }
+// if (max < Num1)
+// {
+//     max = Num1;
+// }
+// Console.WriteLine("Максимальным являеться число " + max);
+
+
+// Console.WriteLine("Введите два числа ");
+// int Number1 = Convert.ToInt32(Console.ReadLine());
+// int Number2 = Convert.ToInt32(Console.ReadLine());
+// int max = Number1;
+// int min = Number2;
+// if (Number1 < Number2)
+// {
+//     max = Number2;
+//     min = Number1;
+// }
+// Console.WriteLine("Максимальным являеться число " + max + " Минимальным являеться число " + min);
