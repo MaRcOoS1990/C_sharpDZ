cce18bd2c0ede749a6c30ec644a46d643809b1fb
MaRcOoS <leha.chalov@mail.ru>
2023-07-04 23:02:17 +0300

    Семинар 2

--- Program.cs
+++ Program.cs
@@ -68,3 +68,61 @@
 //     min = Number1;
 // }
 // Console.WriteLine("Максимальным являеться число " + max + " Минимальным являеться число " + min);
+
+
+// int randomNumber = new Random().Next(10, 100);
+// Console.WriteLine(randomNumber);
+// int firstDigit = randomNumber / 10;
+// int secondDigit = randomNumber % 10;
+// int max = firstDigit;
+// if (secondDigit > max)
+// {
+//     max = secondDigit;
+// }
+// Console.WriteLine($"Максимальным являеться число  {max}");
+
+
+// int randomNumber = new Random().Next(100, 1000);
+// Console.WriteLine(randomNumber);
+// int firstDigit = randomNumber / 100;
+// int secondDigit = randomNumber % 10;
+// Console.WriteLine($"{firstDigit}{secondDigit}");
+
+// int randomNumber = new Random().Next(100, 1000);
+// int result = (randomNumber / 100) * 10 + randomNumber % 10;
+// Console.WriteLine($"{randomNumber}  {result}");
+
+
+// int randomNumber = new Random().Next(100, 1000);
+// string num = Convert.ToString(randomNumber);
+// Console.WriteLine($"{randomNumber}  {num[0]}{num[2]}");
+
+
+
+// Console.WriteLine("Введите два числа ");
+// int Number1 = Convert.ToInt32(Console.ReadLine());
+// int Number2 = Convert.ToInt32(Console.ReadLine());
+// if (Number1 % Number2 == 0)
+// {
+//     Console.WriteLine("Первое число кратно второму");
+// }
+// else
+// {
+//     int num = Number1 % Number2;
+//     Console.WriteLine("Первое число не кратно второму, остаток = " + num);
+// }
+
+
+// Console.WriteLine("Введите число ");
+// int Number1 = Convert.ToInt32(Console.ReadLine());
+// if (Number1 % 7 == 0 && Number1 % 23 == 0)
+// {
+//     Console.WriteLine($"Число {Number1} делиться без остатка на 7 и 23");
+// }
+// else
+// {
+//     Console.WriteLine($"Число {Number1} делиться на 7 с остатком { Number1 % 7}");
+//     Console.WriteLine($"Число {Number1} делиться на 23 с остатком { Number1 % 23}");
+// }
+
+
