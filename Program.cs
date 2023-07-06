96486e33755e2421e394e498b0ad996ec6d32105
MaRcOoS <leha.chalov@mail.ru>
2023-07-06 18:41:01 +0300

    Третья домашня работа1

--- Program.cs
+++ Program.cs
@@ -289,6 +289,42 @@
 // Console.WriteLine("Число не является палиндромом");
 // }
 
+// второй вариант
+
+// Console.Write("Введите пятизначное число или строку из 5 символов: ");
+// string? polin= Console.ReadLine();
+// int number;
+// bool isNumber = int.TryParse(polin, out number);
+// if (isNumber)
+// {
+//     Console.WriteLine($"в строчке {polin} находятся только цифры");
+//     int polinD = Convert.ToInt32(polin);
+//     if (polinD < 10000 || polinD > 100000)
+// {
+//     Console.WriteLine("Было сказанно ввести пятизначное число");
+//     return;
+// }
+// if (polinD / 10000 == polinD % 10 && polinD % 10000 / 1000 == polinD % 100 / 10)
+// {
+//     Console.WriteLine("Число является палиндромом");
+// }
+// else
+// {
+// Console.WriteLine("Число не является палиндромом");
+// }
+// }
+// else
+// {
+//     Console.WriteLine($"в строчке {polin} находятся символы");
+// }
+// static string Reverse( string Cpolin )
+// {
+//     char[] polin = Cpolin.ToCharArray();
+//     Array.Reverse(polin);
+//     return new string(polin);
+// }
+
+
 
 // Console.Write("Введите координату по оси оХ для первой точки: ");
 // int x1 = Convert.ToInt32(Console.ReadLine());
@@ -307,12 +343,18 @@
 // Console.Write($"Расстояние: {d}");
 
 
-Console.WriteLine("Введите число :");
-int start;
-int end = Convert.ToInt32(Console.ReadLine());
-for (start = 1; start <= end; start++)
-{
-    Console.Write(Math.Pow(start, 3) + " ");
-}
+// Console.WriteLine("Введите число :");
+// int start;
+// int end = Convert.ToInt32(Console.ReadLine());
+// for (start = 1; start <= end; start++)
+// {
+//     Console.Write(Math.Pow(start, 3) + " ");
+// }
+
+
+
+
+
+// Семинар 4
 
 
