5bb80e821a46c01a2e59f300ca98e12b029b3a29
MaRcOoS <leha.chalov@mail.ru>
2023-07-07 09:00:28 +0300

    Домашняя работа 4

--- DZ_4.cs
+++ DZ_4.cs
@@ -17,9 +17,47 @@
 
 // Console.Write("Введите число : ");
 // int num = Convert.ToInt32(Console.ReadLine());
-// if (num >0)
+// int len = NumberLen(num);
+// SumNumbers(num, len);
+// int NumberLen(int a)
 // {
-//     int result = num / 100 + num % 100 / 10 + num % 10;
+//     int index = 0;
+//     while (a > 0)
+//     {
+//         a /= 10;
+//         index++;
+//     }
+//     return index;
+// }
+// void SumNumbers(int n, int len)
+// {
+//     int sum = 0;
+//     for (int i = 1; i <= len; i++)
+//     {
+//         sum += n % 10;
+//         n /= 10;
+//     }
+//     Console.WriteLine($"Сумма цифр в числе {num} = {sum}");
 // }
 
-// Console.WriteLine(result);
+
+
+// int[] array = new int[8];
+// Random rand = new Random();
+// for (int i = 0; i < array.Length; i++)
+// {
+//      array[i] = rand.Next(10, 1000);
+// }
+// Console.WriteLine($"Массив : [ {string.Join("; ", array)} ]");
+
+
+// int num;
+// int[] array = new int[8];
+// num = 0;
+// do
+// {
+//     array[num] = Convert.ToInt32(Console.ReadLine());
+//     num++;
+// }
+// while (num < 8);
+// Console.WriteLine($"Массив : [ {string.Join("; ", array)} ]");
