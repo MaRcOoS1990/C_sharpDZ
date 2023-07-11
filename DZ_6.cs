c652a7a7ad7db41c57baf90a974834d523b4e664
MaRcOoS <leha.chalov@mail.ru>
2023-07-11 15:12:35 +0300

    6 домашнее задание

---
+++ DZ_6.cs
@@ -0,0 +1,37 @@
+// Console.WriteLine("Введите размер массива :");
+// int sizeArray = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите числа :");
+// int count = 0;
+// int[] array = new int[sizeArray];
+// for (int i = 0; i < sizeArray; i++)
+// {
+//     array[i] = Convert.ToInt32(Console.ReadLine());
+//     if (array[i] > 0)
+//     {
+//         count++;
+//     }
+// }
+// Console.WriteLine($"Вы ввели масcив : [ {string.Join("; ", array)} ]");
+// Console.WriteLine($"Число положительных чисел в масиве = {count}");
+
+
+// Console.WriteLine("Давайте найдем точку пересения двух прямых");
+// Console.WriteLine("Введите точку b1 :");
+// double b1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите точку b2 :");
+// double b2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите точку k1 :");
+// double k1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите точку k2 :");
+// double k2 = Convert.ToInt32(Console.ReadLine());
+// double dot1 = k1 * (b2 - b1) / (k1 - k2) + b1;
+// double dot2 = k2 * (b2 - b1) / (k1 - k2) + b2;
+// if (dot1 == dot2)
+// {
+//     Console.WriteLine($"Точка пересечения двух прямых = {dot1} ; {dot2}");
+// }
+// else
+// {
+//     Console.WriteLine("Прямые не пересекаються");
+// }
+
