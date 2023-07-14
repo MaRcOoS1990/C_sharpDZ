2461293b14a8b447bb571617e38db1fe5d8f3770
MaRcOoS <leha.chalov@mail.ru>
2023-07-14 18:48:09 +0300

    Решено 40 задач

--- 400Task.cs
+++ 400Task.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 // Console.Write("Введите пятизначное число или строку из 5 символов: ");
 // string? polin = Console.ReadLine();
 // int number;
@@ -233,3 +234,140 @@ using System;
 
 // *25
 // Console.WriteLine("Введите сторону куба: ");
+// int cubeV = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"Объм куба = {cubeV * cubeV * cubeV} и площадь одной стороны куб = {cubeV * 6}");
+
+// *26
+// Console.WriteLine("Сколько кг конфет купим?: ");
+// int candys = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Сколько кг печенья купим?: ");
+// int coocies = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Сколько кг яблок купим?: ");
+// int apples = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите стоимость 1 кг всех продуктов: ");
+// int cost = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"Стоимость всей покупки = {(candys + coocies + apples) * cost}");
+
+// *27
+// Console.WriteLine("Введите число 1: ");
+// double number1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число 2: ");
+// double number2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"Сумма = {number1 + number2}\nРазность = {number1 - number2}\nПроизведение = {number1 * number2}\nДеление 1го на 2ое = {number1 / number2}");
+
+// *28
+// Console.WriteLine("Введите количество дней: ");
+// int days = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество месяцев: ");
+// int months = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество лет: ");
+// int yars = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"Всего дней = {months * 30 + yars * 12 * 30 + days}");
+
+// *29
+// double Kelvin = 273.15;
+// double Faringeit = 32;
+// Console.WriteLine("Введите температуру по цельсию: ");
+// double Celsiy = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine($"Температура по цельсию = {Celsiy}\nТемпература по фарингейту = {Celsiy * Faringeit}\nТемпература по кельвину = {Celsiy * Kelvin}");
+
+// *30
+// Console.WriteLine("Введите количество секунд: ");
+// int sec = Convert.ToInt32(Console.ReadLine());
+// int minutes = sec / 60;
+// int newSec = sec - minutes * 60;
+// int hour = minutes / 60;
+// int newMinnutes = minutes - hour * 60;
+// TimeSpan time = new TimeSpan(hour, newMinnutes, newSec);
+// Console.WriteLine("{0} its {1}", sec, time);
+
+// *31
+// Console.WriteLine(543 / 130);
+
+// *32
+// Console.WriteLine("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number < 0)
+// {
+//     return;
+// }
+// Console.WriteLine($"{number % 10}");
+
+// *33
+// Console.WriteLine("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number < 0)
+// {
+//     return;
+// }
+// Console.WriteLine($"{number % 100 / 10}");
+
+// *34
+// Console.WriteLine("Введите число от 1 до 999: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number < 0 || number > 999)
+// {
+//     Console.WriteLine("Нет! Введите число от 1 до 999");
+//     return;
+// }
+// Console.WriteLine($"{number % 10 + number % 100 / 10 + number / 100}");
+
+// *35
+// Console.WriteLine("Введите число от 1000 до 999999: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number < 1000 || number > 999999)
+// {
+//     Console.WriteLine("Нет! Введите число от 1000 до 999999");
+//     return;
+// }
+// else
+// {
+//     number = number / 1000;
+// }
+
+// Console.WriteLine($"{number % 10 + number % 100 / 10 + number / 100}");
+
+// *36
+// Console.WriteLine("Введите число от 1 до 999: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number < 0 || number > 999)
+// {
+//     Console.WriteLine("Нет! Введите число от 1 до 999");
+//     return;
+// }
+// Console.WriteLine($"Число единиц = {number % 10}\nЧисло десятков = {number % 100 / 10}\nПроизведение его цифр = {number % 10 * (number % 100 / 10) * (number / 100)}\nСумма его цифр = {number % 10 + number % 100 / 10 + number / 100}");
+
+// *37
+// Console.WriteLine("Введите число от 1 до 999: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number < 0 || number > 999)
+// {
+//     Console.WriteLine("Нет! Введите число от 1 до 999");
+//     return;
+// }
+// int a = number / 100;
+// int b = number % 100 / 10;
+// int c = number % 10;
+// Console.WriteLine($"{c}{b}{a}\n{b}{c}{a}\n{c}{a}{b}\n{b}{a}{c}\n{a}{c}{b}");
+
+// *38
+// Console.Write("Введите число от 1 до 999: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number < 0 || number > 999)
+// {
+//     Console.WriteLine("Нет! Введите число от 1 до 999");
+//     return;
+// }
+// int a = number / 100;
+// int b = number % 100 / 10;
+// int c = number % 10;
+// Console.WriteLine($"{a}{b}{c}\n{b}{c}{a}\n{c}{a}{b}\n{a}{c}{b}\n{b}{a}{c}\n{c}{b}{a}");
+
+// *39(Вещественные числа)
+// Console.Write("Введите первое число: ");
+// double num1 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// double num2 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine($"{(num1 + num2) / 2}");
+
+// *40
