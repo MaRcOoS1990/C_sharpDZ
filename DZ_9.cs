bf28d376bd2270164e3129cd46b50b791d1bed83
MaRcOoS <leha.chalov@mail.ru>
2023-07-22 13:02:44 +0300

    Домашняя работа номер девять

--- DZ_9.cs
+++ DZ_9.cs
@@ -24,22 +24,16 @@
 
 
 
-// Console.Write("Введите число N :");
-// int n = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Введите число M :");
-// int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N :");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число M :");
+int m = Convert.ToInt32(Console.ReadLine());
 
 
-// int СonsiderAckkerman(int n, int m)
-// {
-//     if (n == 0) return m + 1;
-//     if (m == 0)
-//     {
-//         return СonsiderAckkerman(n - 1, 1);
-//     }
-//     else
-//     {
-//         return СonsiderAckkerman(n - 1, СonsiderAckkerman(n, m - 1));
-//     }
-// }
-// Console.WriteLine($"Аккерман = {СonsiderAckkerman(m, n)}");
+int СonsiderAckkerman(int n, int m)
+{
+    if (n == 0) return m + 1;
+    else if (m == 0) return СonsiderAckkerman(n - 1, 1);
+    else return СonsiderAckkerman(n - 1, СonsiderAckkerman(n, m - 1));
+}
+Console.WriteLine($"Аккерман = {СonsiderAckkerman(m, n)}");
